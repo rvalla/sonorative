5692b3ed7e69543fc1c9e88e4c69266a48db1ee9
Rodrigo Valla <rodrigovalla@protonmail.ch>
2023-10-09 08:14:15 -0300

    colorssound: Improving, new spectral mode...

--- README.md
+++ README.md
@@ -2,7 +2,23 @@
 
 # sonorative
 
-Here I am exploring options to transform color data in images into sound... Work in progress!
+Here I am exploring options to transform data from images into sound. The idea is to test
+different algorithms and play (I am talking about having fun and also listening to the output
+audio files). This is not my priority project now but I came back to it from time to time.  
+
+## ColorsSound()
+
+With **ColorsSound()** I explore different ways of taking an image color data (grayscale or
+rgb values) and map it to the amplitude of an audio signal. I am working in 3 specific
+*modes* in this stage:
+
+- **amplitude**: the amplitude of the signal is directly taken from pixel data (some sort of *am synthesis*).
+- **frequency**: the amplitude of the signal depends on the output of a sine functions which frequency is the
+color value (some sort of *fm synthesis*).
+- **sceptral**: the amplitude of the signal depends on the combinations of the amplitudes from a set of partials
+(3 different frequencies, one for each color). Some sort of spectral modulation (ok, this doesn't exist).  
+
+You can check the results at */colorssound/output*.  
 
 Feel free to contact me by [mail](mailto:rodrigovalla@protonmail.ch) or reach me in
 [telegram](https://t.me/rvalla) or [mastodon](https://fosstodon.org/@rvalla).
--- colorssound/colorssound.py
+++ colorssound/colorssound.py
@@ -6,13 +6,13 @@ from PIL import Image as im
 from scipy.io import wavfile as wf
 
 class ColorsSound():
-	"Blending images one on top of the other"
+	"Creating sound from the colors in an image"
 
 	def __init__(self, configpath):
 		self.config = js.load(open(configpath))
-		self.starttime = None
-		self.outPath = None
-		self.outFile = None
+		self.start_time = None
+		self.out_path = None
+		self.out_file = None
 		self.mode = None
 		self.direction = None
 		self.imagedata = None
@@ -20,40 +20,60 @@ class ColorsSound():
 		self.audiosr = None
 		self.audiolength = None
 		self.audiodata = None
-		ColorsSound.setConfig(self, self.config)
+		self.frequency_factor = None
+		self.partials = None
+		self.set_config(self.config)
 		print(self)
 		if self.mode == "amplitude":
-			ColorsSound.runAmp(self)
+			self.run_amp()
 		elif self.mode == "frequency":
-			ColorsSound.runFreq(self)
-		self.audiomax = ColorsSound.getMaxAmplitude(self.audiodata)
-		ColorsSound.save(self.outPath, self.outFile, self.audiosr, self.audiodata, self.audiomax)
+			self.run_freq()
+		elif self.mode == "spectral":
+			self.run_spectral()
+		self.audiomax = self.max_amplitude(self.audiodata)
+		self.save(self.out_path, self.out_file, self.audiosr, self.audiodata, self.audiomax)
 		print("-- time needed for extracting the mistery sound: " + \
-				ColorsSound.getWorkingTime(self.starttime, tm.time()))
+				self.working_time(self.start_time, tm.time()))
 
-	def runAmp(self):
-		self.starttime = tm.time()
+	#Running in amplitude mode...
+	def run_amp(self):
+		self.start_time = tm.time()
 		if self.direction == "horizontal":
-			ColorsSound.mapAmp(self.imagedata, self.audiodata, 0)
+			self.map_amplitude(self.imagedata, self.audiodata, 0)
 		elif self.direction == "vertical":
-			ColorsSound.mapAmp(self.imagedata, self.audiodata, 1)
+			self.map_amplitude(self.imagedata, self.audiodata, 1)
 		elif self.direction == "both":
-			ColorsSound.mapAmp(self.imagedata, self.audiodata, 2)
+			self.map_amplitude(self.imagedata, self.audiodata, 2)
 		else:
 			print("-- I don't know what to do with this mode...", end="\n")
 
-	def runFreq(self):
-		self.starttime = tm.time()
+	#Running in frequency mode...
+	def run_freq(self):
+		self.start_time = tm.time()
 		if self.direction == "horizontal":
-			ColorsSound.mapFreq(self.imagedata, self.audiodata, 0)
+			self.map_frequency(self.imagedata, self.audiodata, 0)
 		elif self.direction == "vertical":
-			ColorsSound.mapFreq(self.imagedata, self.audiodata, 1)
+			self.map_frequency(self.imagedata, self.audiodata, 1)
 		elif self.direction == "both":
-			ColorsSound.mapFreq(self.imagedata, self.audiodata, 2)
+			self.map_frequency(self.imagedata, self.audiodata, 2)
+		else:
+			print("-- I don't know what to do with this mode...", end="\n")
+	
+	#Running spectral mode...
+	def run_spectral(self):
+		pass
+		self.start_time = tm.time()
+		if self.direction == "horizontal":
+			self.map_spectrum(self.imagedata, self.audiodata, self.partials, 0)
+		elif self.direction == "vertical":
+			self.map_spectrum(self.imagedata, self.audiodata, self.partials, 1)
+		elif self.direction == "both":
+			self.map_spectrum(self.imagedata, self.audiodata, self.partials, 2)
 		else:
 			print("-- I don't know what to do with this mode...", end="\n")
 
-	def mapAmp(idata, adata, mode):
+	#Deciding amplitude for each sample based in pixel color...
+	def map_amplitude(self, idata, adata, mode):
 		print("-- mapping colors to amplitude...", end="\r")
 		w = idata.shape[0]
 		h = idata.shape[1]
@@ -61,94 +81,142 @@ class ColorsSound():
 			for y in range(h):
 				if mode == 0:
 					s = x * h + y
-					adata[s] +=  ColorsSound.getAmplitude(idata[x][y])
+					adata[s] +=  self.get_amplitude(idata[x][y])
 				elif mode == 1:
 					s = x + y * w
-					adata[s] -= ColorsSound.getAmplitude(idata[x][y])
+					adata[s] += self.get_amplitude(idata[x][y])
 				elif mode == 2:
 					s = x * h + y
-					adata[s] += ColorsSound.getAmplitude(idata[x][y])
+					adata[s] += self.get_amplitude(idata[x][y])
 					s = x + y * w
-					adata[s] -= ColorsSound.getAmplitude(idata[x][y])
+					adata[s] -= self.get_amplitude(idata[x][y])
 		print("-- audio data is ready!             ", end= "\n")
 
-	def getAmplitude(pixeldata):
+	def get_amplitude(self, pixeldata):
 		if isinstance(pixeldata,np.ndarray):
-			a = pixeldata[0] * 0.45 + pixeldata[1] * 0.32 + pixeldata[2] * 0.23
-			a = a / 255
+			a = -255 + 2 * (pixeldata[0] * 0.45 + pixeldata[1] * 0.32 + pixeldata[2] * 0.23)
 			return a
 		elif isinstance(pixeldata,np.uint8):
-			a = pixeldata / 255
+			a = -255 + 2 * pixeldata
 			return a
 		else:
 			print("-- something strange happens with pixel's data...")
 			return 0
 
-	def mapFreq(idata, adata, mode):
+	#Getting amplidute for sample using color value as frequency input...
+	def map_frequency(self, idata, adata, mode):
 		print("-- mapping colors to amplitude...", end="\r")
 		w = idata.shape[0]
 		h = idata.shape[1]
+		step_w = 2 * math.pi / (w * self.frequency_factor)
+		step_h = 2 * math.pi / (h * self.frequency_factor)
 		for x in range(w):
 			for y in range(h):
 				if mode == 0:
 					s = x * h + y
-					adata[s] +=  ColorsSound.getFreqAmplitude(idata[x][y])
+					adata[s] +=  self.get_freq_amplitude(s, step_w, idata[x][y])
 				elif mode == 1:
 					s = x + y * w
-					adata[s] -= ColorsSound.getFreqAmplitude(idata[x][y])
+					adata[s] += self.get_freq_amplitude(s, step_h, idata[x][y])
 				elif mode == 2:
 					s = x * h + y
-					adata[s] += ColorsSound.getFreqAmplitude(idata[x][y])
+					adata[s] += self.get_freq_amplitude(s, step_w, idata[x][y])
 					s = x + y * w
-					adata[s] -= ColorsSound.getFreqAmplitude(idata[x][y])
+					adata[s] -= self.get_freq_amplitude(s, step_h, idata[x][y])
 		print("-- audio data is ready!             ", end= "\n")
 
-	def getFreqAmplitude(pixeldata):
+	def get_freq_amplitude(self, sample, step, pixeldata):
+		angle = sample * step
 		if isinstance(pixeldata,np.ndarray):
-			a = math.sin(pixeldata[0] * 4) + math.sin(pixeldata[1] * 8) + math.sin(pixeldata[2] * 16)
-			a = a
+			a = math.sin(pixeldata[0] * angle) * 0.45 + math.sin(pixeldata[1] * angle) * 0.32 + math.sin(pixeldata[2] * angle) * 0.23
 			return a
 		elif isinstance(pixeldata,np.uint8):
-			a = math.sin(pixeldata * 4)
+			a = math.sin(pixeldata * angle)
+			return a
+		else:
+			print("-- something strange happens with pixel's data...")
+			return 0
+	
+	#Getting amplitude for each sample based giving each partial its amplitude from pixel data...
+	def map_spectrum(self, idata, adata, partials, mode):
+		print("-- mapping colors to amplitude...", end="\r")
+		w = idata.shape[0]
+		h = idata.shape[1]
+		step_w = 2 * math.pi / (w * self.frequency_factor)
+		step_h = 2 * math.pi / (h * self.frequency_factor)
+		for x in range(w):
+			for y in range(h):
+				if mode == 0:
+					s = x * h + y
+					adata[s] +=  self.get_spec_amplitude(s, step_w, partials, idata[x][y])
+				elif mode == 1:
+					s = x + y * w
+					adata[s] += self.get_spec_amplitude(s, step_h, partials, idata[x][y])
+				elif mode == 2:
+					s = x * h + y
+					adata[s] += self.get_spec_amplitude(s, step_w, partials, idata[x][y])
+					s = x + y * w
+					adata[s] -= self.get_spec_amplitude(s, step_h, partials, idata[x][y])
+		print("-- audio data is ready!             ", end= "\n")
+	
+	def get_spec_amplitude(self, sample, step, partials, pixeldata):
+		angle = sample * step
+		if isinstance(pixeldata,np.ndarray):
+			a = math.sin(partials[0] * angle) * pixeldata[0] * 0.45
+			a += math.sin(partials[1] * angle) * pixeldata[1] * 0.32
+			a += math.sin(partials[2] * angle) * pixeldata[2] * 0.23
+			return a
+		elif isinstance(pixeldata,np.uint8):
+			a = math.sin(partials[0] * angle) * pixeldata
 			return a
 		else:
 			print("-- something strange happens with pixel's data...")
 			return 0
 
-	def getMaxAmplitude(adata):
+	#Getting the maximum amplitude in adata...
+	def max_amplitude(self, adata):
 		return np.amax(np.absolute(adata))
 
-	def save(filepath, filename, sr, adata, maxamp):
+	#Saving our work to an audio file...
+	def save(self, filepath, filename, sr, adata, maxamp):
 		adata = adata / maxamp
 		wf.write(filepath + filename + ".wav", sr, adata)
 		print("-- output audio file saved!", end="\n")
 
-	def setConfig(self, data):
+	#Loading configuration data from configuration file...
+	def set_config(self, data):
 		self.mode = data["mode"]
 		self.direction = data["direction"]
-		self.outPath = data["outPath"]
-		self.outFile = data["outFile"]
+		self.out_path = data["outPath"]
+		self.out_file = data["outFile"]
 		self.imagedata = np.array(im.open(data["inPath"] + data["inFile"]))
 		self.imagesize.append(self.imagedata.shape[0])
 		self.imagesize.append(self.imagedata.shape[1])
 		self.audiosr = data["sampleRate"]
-		self.audiolength = ColorsSound.getAudioLength(self.imagesize)
+		self.audiolength = self.get_audio_length(self.imagesize)
 		self.audiodata = np.zeros((self.audiolength), dtype="float32")
-		self.startTime = tm.time()
+		self.frequency_factor = data["fModeFactor"]
+		self.partials = self.get_partials(data["partials"])
+		self.start_time = tm.time()
+	
+	#Loading partials frequency from configuration file...
+	def get_partials(self, data):
+		return [int(a) for a in data.split(",")]
 
-	def getAudioLength(isize):
+	#Getting the total number of audio samples...
+	def get_audio_length(self, isize):
 		n = 1
 		for f in range(len(isize)):
 			n = n * isize[f]
 		return n
 
 	#Calculating time needed for processing an image...
-	def getWorkingTime(start, end):
+	def working_time(self, start, end):
 		time = end - start
-		formatedTime = ColorsSound.formatTime(time)
-		return formatedTime
-
-	def formatTime(time):
+		formated_time = self.formatTime(time)
+		return formated_time
+	
+	def formatTime(self, time):
 		ms = ""
 		minutes = time // 60
 		seconds = time - minutes * 60
